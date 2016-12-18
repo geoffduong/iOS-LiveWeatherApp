9e28bc207b8fb2be51e7b6bfc2dedf4353e73aba
mrricearoni <duongg4848@gmail.com>
2016-12-18 17:57:24 -0500

    WeatherCondition is now being retrieved from JSON
    ADDED
    updateWeatherTable function is now correctly parsing JSON for the weather condition and appending to the corresponding array.
    
    IN PROGRESS
    If cell is tapped, bring up detailed weather info with picture

--- LiveWeatherApp/LiveWeatherApp/ViewController.swift
+++ LiveWeatherApp/LiveWeatherApp/ViewController.swift
@@ -16,11 +16,6 @@ class ViewController: UITableViewController {
     //----------------------------------------------------------
     
     //TEST ARRAYS-----------------------------------------------
-//    var cv_zipCode: [String] = ["48197", "85365", "99703"]
-//    var cv_cityName: [String] = ["Ypsilanti", "Yuma", "Fort Wainwright"]
-//    var cv_state: [String] = ["MI", "AZ", "AK"]
-//    var cv_temp: [String] = ["55", "75", "34"]
-//    var cv_weatherCondition: [String] = ["Snow", "Rain", "Clear"]
     var cv_zipCode: [String] = []
     var cv_cityName: [String] = []
     var cv_state: [String] = []
@@ -33,11 +28,6 @@ class ViewController: UITableViewController {
     //Onload----------------------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hello")
-        let owm_url = URL(string: "http://api.openweathermap.org/data/2.5/weather?zip=48176,us&appid=d5bf1afd8b284f2ac9ea60d9d5a2557e&units=imperial")!
-        let owm_data : Data = try! Data(contentsOf: owm_url)
-        let owm_json = try! JSONSerialization.jsonObject(with: owm_data, options: .allowFragments) as! [String:AnyObject]
-        print(owm_json)
         updateWeatherTable()
     }
     
@@ -61,7 +51,7 @@ class ViewController: UITableViewController {
         cell.vv_lblCityName?.text = self.cv_cityName[(indexPath as NSIndexPath).row]
         cell.vv_lblZipCode?.text = self.cv_zipCode[(indexPath as NSIndexPath).row]
         cell.vv_lblState?.text = self.cv_state[(indexPath as NSIndexPath).row]
-        cell.vv_lblTemp?.text = self.cv_temp[(indexPath as NSIndexPath).row] + "\u{00B0}"
+        cell.vv_lblTemp?.text = self.cv_temp[(indexPath as NSIndexPath).row] + "\u{00B0}F"
         
         return cell
     }
@@ -85,10 +75,16 @@ class ViewController: UITableViewController {
     
     
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue){
-        //updateWeatherTable()
+        cv_temp.removeAll()
+        cv_weatherCondition.removeAll()
+        cv_state.removeAll()
+        cv_zipCode.removeAll()
+        cv_cityName.removeAll()
+        updateWeatherTable()
         weatherTableView.reloadData()
     }
     
+    //Updates arrays based on JSON
     func updateWeatherTable() {
         db = WeatherDatabase()
         let test = db?.queryZipCodes()
@@ -111,6 +107,7 @@ class ViewController: UITableViewController {
             let owm_data : Data = try! Data(contentsOf: owm_url)
             let owm_json = try! JSONSerialization.jsonObject(with: owm_data, options: .allowFragments) as! [String: AnyObject]
             
+            //Get temperature
             if let json = owm_json as? [String: AnyObject] {
                 if let main = json["main"] as? [String: AnyObject] {
                     if let temp = main["temp"] as? NSNumber {
@@ -118,20 +115,17 @@ class ViewController: UITableViewController {
                     }
                 }
             }
-        
-//            let owm_json = try! JSONSerialization.jsonObject(with: owm_data, options: .allowFragments) as! [String:AnyObject]
-//            cv_temp.append("47")
-//            cv_weatherCondition.append("Clear")
             
-//            if let nestedDictionary = owm_json["main"] as! [String: Any] {
-//                cv_temp.append(nestedDictionary.)
-//            }
-//
-//            if let array = owm_json["weather"] as? [String: Any] {
-//                if let weather = array.first {
-//                    cv_weatherCondition.append(weather.value as! String)
-//                }
-//            }
+            //Get weather description
+            if let json = owm_json as? [String: AnyObject] {
+                if let weather = json["weather"] as? NSArray {
+                    if let nestedDictionary = weather[0] as? NSDictionary {
+                        if let weatherDescription = nestedDictionary["main"] as? String {
+                            cv_weatherCondition.append(weatherDescription)
+                        }
+                    }
+                }
+            }
         }
     }
 }
